764b0f10f281bd4a3034ef9dcd9fa35c1ab9e6b8
Kareem Sabra <kareem20sabra@gmail.com>
2020-09-27 15:13:36 +0200

    4th version

---
+++ site/js/script.js
@@ -0,0 +1,24 @@
+function openPerson(evt, personName) {
+    
+    var i, tabcontent, tablinks;
+  
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    
+    document.getElementById(personName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+ var x = document.getElementById("homeblock");
+    x.style.display = "none";
+  
+  }
